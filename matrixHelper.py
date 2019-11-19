4554b748ebf8c70d2b1f20c5b150f84429881497
Richard Padilla <padillarichard2021@berkeley.edu>
2019-11-18 21:51:54 -0800

    added new file and one input

---
+++ matrixHelper.py
@@ -0,0 +1,26 @@
+def matrixExpander(m):
+    matrix=m.copy()
+    #expanding horizontally
+    for i in range (len(matrix)):
+        matrix[i]=matrix[i]+matrix[i][::-1]
+    #exexpanding vertically
+    for i in range(len(matrix)):
+        inverseMatrix=matrix.copy()[::-1]
+    matrix+=inverseMatrix
+    return matrix
+def printMatrix(m):
+    for i in range(len(m)):
+        for j in range(len(m)):
+            print(m[i][j],end=' ')
+        print('\n')
+matrix5=matrixExpander([[0,1,3,5,5],[1,0,2,4,5],[3,2,0,3,6],[5,4,3,0,4],[5,5,6,4,0]])
+matrix10=matrixExpander(matrix5)
+matrix20=matrixExpander(matrix10)
+matrix40=matrixExpander(matrix20)
+for i in range(len(matrix20)):
+    for j in range(len(matrix20)):
+        if matrix20[i][j] == 0:
+            matrix20[i][j] = 'x'
+print(len(matrix20))
+print(len(matrix20[1]))
+printMatrix(matrix20)
